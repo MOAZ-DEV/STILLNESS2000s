27ad84814a447a551e480e56c96f7163de4162be
MoaazAllaEden <moaz.dev@yahoo.com>
2024-10-01 16:16:01 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,27 @@ const port = process.env.PORT || 3000;
 app.get('/', async (req, res) => {
   console.log('Cron job started at:', new Date().toISOString());
   res.status(200).write('Cron job started at: ' + new Date().toISOString());
-  GET().catch((err) => {
+  Post().catch((err) => {
     res.status(400).emit('Error occurred during cron job:' + err + ' at: ' + new Date().toISOString())
   });
 })
-export async function GET(request?: Request) {
+export function GET(request?: Request) {
+  for (let i = 0; i < 3; i++) {
+    setTimeout(() => {
+      Post();
+    }, 200);
+  }
+};
+
+const Post = async () => {
   try {
     const geminiImagePrompt = await useGemini({ prompt: prompt });
     const geminiCaption = await useGemini({ prompt: caption });
     const pollinationsResult = await usePollinations(geminiImagePrompt.result, 3000, 3000, 'turbo', 24);
     await useInstagram(accessToken, userId, pollinationsResult.res, geminiCaption.result)
-      .catch(err =>{
+      .catch(err => {
+        GET();
+        GET();
         GET();
       });
   } catch (error) {
@@ -28,8 +38,7 @@ export async function GET(request?: Request) {
     console.error('Error occurred during cron job:', error);
     GET();
   }
-};
-
+}
 app.listen(port, () => {
   console.clear();
   console.log(`Server is running on port ${port}`);
