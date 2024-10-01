f1d4ee1b3b9006c0bbcac5c9955c30069bd6d575
MoaazAllaEden <moaz.dev@yahoo.com>
2024-10-01 19:58:29 +0300

    Added the process

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,11 @@ import * as middlewares from './middlewares';
 import api from './api';
 import MessageResponse from './interfaces/MessageResponse';
 
+import { useGemini } from './hooks/useGemini';
+import { usePollinations } from './hooks/usePollinations';
+import { useInstagram } from './hooks/useInstagram';
+import { accessToken, userId, prompt, caption } from './shared';
+
 require('dotenv').config();
 
 const app = express();
@@ -16,11 +21,32 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+const Post = async () => {
+  try {
+    const geminiImagePrompt = await useGemini({ prompt: prompt });
+    const geminiCaption = await useGemini({ prompt: caption });
+    const pollinationsResult = await usePollinations(geminiImagePrompt.result, 3000, 3000, 'turbo', 24);
+    await useInstagram(accessToken, userId, pollinationsResult.res, geminiCaption.result)
+      .catch(() => {
+        Post();
+      });
+  } catch (error) {
+    console.clear();
+    console.error('Error occurred during cron job:', error);
+    Post();
+  }
+};
+
 app.get<{}, MessageResponse>('/', (req, res) => {
+  Post().catch((err) => {
+    res.status(400).emit('Error occurred during cron job:' + err + ' at: ' + new Date().toISOString());
+  });
   res.json({
-    message: 'I\'m so happy cuz it worked out!',
+    message: 'Process started at: ' + new Date().toISOString,
   });
 });
+
+
 
 app.use('/api/v1', api);
 
