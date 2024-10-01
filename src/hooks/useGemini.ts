5ca47944a7c0da5957373c1239f1baca532874b5
MoaazAllaEden <moaz.dev@yahoo.com>
2024-10-01 19:24:10 +0300

    refactor test

---
+++ src/hooks/useGemini.ts
@@ -0,0 +1,20 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+interface useGeminiProps {
+    prompt: string;
+}
+
+const API_KEY = "AIzaSyDzJEmVkZejnnIkIGcPI0_oatFnei3phDo";
+export const useGemini = async ({ prompt }: useGeminiProps) => {
+
+    console.clear();
+    console.log(prompt);
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const result = await model.generateContent(prompt);
+
+    return {
+        result: result.response.text()
+    }
+}
