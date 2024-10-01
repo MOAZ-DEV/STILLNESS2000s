19306e05bc5f845992e0583bf8dd9c2bacb093e0
MoaazAllaEden <moaz.dev@yahoo.com>
2024-10-01 20:20:49 +0300

    update

--- src/app.ts
+++ src/app.ts
@@ -27,19 +27,19 @@ const Post = async () => {
     const geminiCaption = await useGemini({ prompt: caption });
     const pollinationsResult = await usePollinations(geminiImagePrompt.result, 3000, 3000, 'turbo', 24);
     await useInstagram(accessToken, userId, pollinationsResult.res, geminiCaption.result)
-      .catch(() => {
-        Post();
+      .catch((error) => {
+        console.error('Error occurred during Instagram posting:', error);
       });
   } catch (error) {
     console.clear();
     console.error('Error occurred during cron job:', error);
-    Post();
   }
 };
 
 app.get<{}, MessageResponse>('/', (req, res) => {
+  const time = new Date().toISOString();
   Post().catch((err) => {
-    res.status(400).emit('Error occurred during cron job:' + err + ' at: ' + new Date().toISOString());
+    res.status(400).emit('Error occurred during cron job:' + err + ' at: ' + time);
   });
   res.json({
     message: 'Process started at: ' + new Date().toISOString,
